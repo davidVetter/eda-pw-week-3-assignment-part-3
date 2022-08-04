e73c8b5f75e4ff6016a06c7830ab663e8e7c039c
David Vetter <ddvetter23@gmail.com>
2022-08-04 13:13:44 -0500

    answered #4

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -19,6 +19,8 @@ console.log(supplyChanges[1]);
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
+let removedItem = supplyChanges.pop();
+console.log(`The removed value was ${removedItem}`);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
